f9dad6b4bb887cb5bb3998a4bb9544064f1a2c68
Alina-Halchenko <104688698+Alina-Halchenko@users.noreply.github.com>
2022-11-26 19:41:30 -0800

    added basic files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { Recipe } from "./Recipe";
+import recipes from '../recipes.json'
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div>
+    <Recipe/>
+    </div>;
 };
+
+
+// style={{
+//   height: '100vh',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+//   fontSize: 40,
+//   color: '#010101'
+// }}
+// >
+// React homework template
