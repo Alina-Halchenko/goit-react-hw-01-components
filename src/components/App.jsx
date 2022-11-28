6dada4dd6e23cd3df027d710b92043fbf88840ff
Alina-Halchenko <104688698+Alina-Halchenko@users.noreply.github.com>
2022-11-27 19:42:18 -0800

    fixed path, started statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,14 @@ import { GlobalStyle } from './GlobalStyle';
 import user from '../user.json';
 import { User } from './User/User';
 
+import data from '../data.json';
+import { Statistics} from './Statistics/Statistics'
+
 export const App = () => {
   return (
     <>
     <User user={user}/>
+    <Statistics title="Upload stats" data={data}/>
       <GlobalStyle/>
     </>
   );
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,5 @@
+
+
+export const Statistics = ({title, user:{id, label, percentage}}) => {
+  
+}
